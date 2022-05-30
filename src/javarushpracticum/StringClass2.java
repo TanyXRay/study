0c5007062110c5f14fd8c7b1b6fc0775627c18ee
User <tany.taray@gmail.com>
2022-05-30 14:25:52 +0400

    Java Rush practicum

---
+++ src/javarushpracticum/StringClass2.java
@@ -0,0 +1,17 @@
+package javarushpracticum;
+
+/*
+пример работы с методами класса String
+ */
+
+import java.util.Scanner;
+
+public class StringClass2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String path = console.nextLine(); // вводим текст с клавиатуры
+
+        String result = path.replace('/', '\\'); // меняем символ в строке на новый
+        System.out.println(result);
+    }
+}
