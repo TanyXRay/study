67b2654e39bd4243cac9dc0423de571c07786f90
User <tany.taray@gmail.com>
2022-06-02 19:15:06 +0400

    Java Rush practicum

---
+++ src/javarushpracticum/StringBuilderClass2.java
@@ -0,0 +1,29 @@
+package javarushpracticum;
+
+/**
+ * Поработаем со StringBuilder
+ */
+
+public class StringBuilderClass2 {
+
+    public static void main(String[] args) {
+        String string = "Учиться, учиться и еще раз учиться! ";
+
+        System.out.println(addTo(string, new String[]{"Под ", "лежачий ", "камень ", "вода ", "не ", "течет"}));
+        System.out.println(replace(string, ", ", 16, 27));
+    }
+
+    public static StringBuilder addTo(String string, String[] strings) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        for (String s : strings) {
+            stringBuilder.append(s);
+        }
+        return stringBuilder;
+    }
+
+    public static StringBuilder replace(String string, String str, int start, int end) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        return stringBuilder.replace(start, end, str);
+    }
+}
+
