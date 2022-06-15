833853610b64f7f7ea314b750e3d4a3031818fd2
User <tany.taray@gmail.com>
2022-06-15 19:04:45 +0400

    task with objects

---
+++ src/study/javarush/practicum/objects/Skyscraper.java
@@ -0,0 +1,36 @@
+package study.javarush.practicum.objects;
+
+/**
+ * Создаем объекты и  их конструкторы.
+ * Построим новый жилой комплекс JavaRush Towers.
+ * 1 конструктор объявляет о результате постройки.
+ * 2 конструктор объявляет о результате и указывает количество этажей.
+ * 3 конструктор объявляет о результате и указывает застройщика.
+ */
+
+public class Skyscraper {
+    public static final String SKYSCRAPER_WAS_BUILD = "Небоскреб построен.";
+    public static final String SKYSCRAPER_WAS_BUILD_FLOORS_COUNT = "Небоскреб построен. Количество этажей - ";
+    public static final String SKYSCRAPER_WAS_BUILD_DEVELOPER = "Небоскреб построен. Застройщик - ";
+
+    public Skyscraper() {
+        System.out.println(SKYSCRAPER_WAS_BUILD);
+    }
+
+    public Skyscraper(int floor) {
+        System.out.println(SKYSCRAPER_WAS_BUILD_FLOORS_COUNT + floor);
+    }
+
+    public Skyscraper(String developer) {
+        System.out.println(SKYSCRAPER_WAS_BUILD_DEVELOPER + developer);
+    }
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+        Skyscraper skyscraperTower = new Skyscraper(50);
+        Skyscraper skyscraperSkyline = new Skyscraper("JavaRushDevelopment");
+    }
+}
+
+
+
