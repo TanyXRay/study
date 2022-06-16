32b3bdaecc3293d6b2d7e241e9901eff163d134c
User <tany.taray@gmail.com>
2022-06-16 13:11:21 +0400

    task with objects

---
+++ src/study/javarush/practicum/objects/Skyscraper2.java
@@ -0,0 +1,31 @@
+package study.javarush.practicum.objects;
+
+/**
+ * Создаем объекты и  их конструкторы.
+ * Нужно будет создать два конструктора: один с параметрами,
+ * второй — без. Они оба должны инициализировать поля floorsCount и developer.
+ * В конструкторе без параметров поля должны инициализироваться значениями "5" и "JavaRushDevelopment".
+ * В конструкторе c параметрами поля должны инициализироваться аргументами этого конструктора.
+ */
+
+public class Skyscraper2 {
+    private int floorsCount;
+    private String developer;
+
+    public Skyscraper2() {
+        floorsCount = 5;
+        developer = "JavaRushDevelopment";
+        System.out.println(floorsCount + developer);
+    }
+
+    public Skyscraper2(int floorsCount, String developer) {
+        this.floorsCount = floorsCount;
+        this.developer = developer;
+        System.out.println(floorsCount + developer);
+    }
+
+    public static void main(String[] args) {
+        Skyscraper2 skyscraper = new Skyscraper2();
+        Skyscraper2 skyscraperUnknown = new Skyscraper2(50, "Неизвестно");
+    }
+}
