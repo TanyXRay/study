c05ca8c637ad7b55852e5056018049f22304d0df
User <tany.taray@gmail.com>
2022-06-20 14:31:42 +0400

    task with static variable

---
+++ src/study/javarush/practicum/statics/static3/Robot.java
@@ -0,0 +1,25 @@
+package study.javarush.practicum.statics.static3;
+
+public class Robot {
+
+    public static int robotCounter;
+
+    public int id;
+
+    public String model;
+
+    public int productionYear;
+
+    public static String vendor = "Javarush Co.";
+
+    public Robot(String model, int productionYear) {
+        this.model = model;
+        this.productionYear = productionYear;
+        this.id = ++robotCounter;
+    }
+
+    public void displayInformation() {
+        System.out.printf("Я %d-й из %d роботов. Модель: %s. Произведен компанией %s в %d году.\n",
+                id, robotCounter, model, vendor, productionYear);
+    }
+}
