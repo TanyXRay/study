fe764d5f12d4421c6c6e851a6f26996c5ca50b4c
User <tany.taray@gmail.com>
2022-06-23 16:53:40 +0400

    task with arrays

---
+++ src/study/javarush/practicum/arrays/Arrays12.java
@@ -0,0 +1,34 @@
+package study.javarush.practicum.arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Работа с массивами.
+ * Найти максимальный элемент в массиве из сгенерированных чисел.
+ */
+
+public class Arrays12 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+       System.out.println(Arrays.stream(array).max()); // 1 способ с помощью класса Arrays метод stream().max()
+
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        System.out.println(max); // 2 способ с помощью проверочной переменной:
+                                 // программа переберет все элементы и если последующий элемент
+                                 // массива будет больше значения, которое лежит под [0] индексом,
+                                 // программа сохранит это число в переменную max и выведет в консоль
+    }
+}
