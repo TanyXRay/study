98cf8c42fa21da41e6d90a6dc82e7100e1a56e74
User <tany.taray@gmail.com>
2022-07-03 13:56:07 +0400

    task with objects

--- src/study/javarush/practicum/inheritance/Main.java
+++ src/study/javarush/practicum/inheritance/Main.java
@@ -1,7 +1,10 @@
 package study.javarush.practicum.inheritance;
 
+import study.javarush.practicum.inheritance.accounts.*;
+import study.javarush.practicum.inheritance.clients.*;
+
 /**
- * Решение задачи по теме: Наследование и инкапсуляция.
+ * Решение задачи №1 по теме: Наследование.Инкапсуляция.Полиморфизм.
  */
 
 public class Main {
@@ -11,23 +14,30 @@ public class Main {
         SavingsAccount savingsAccount = new SavingsAccount("Unknown", 1_000, 500);
         CheckingAccount checkingAccount = new CheckingAccount("Unknown", 1_000, 0);
         CreditAccount creditAccount = new CreditAccount("Unknown", 1_000);
+        Client client = new Client("James", 4);
 
-        savingsAccount.setNameOwner("Bryan");
+        savingsAccount.setNameOwner("James");
         savingsAccount.setBalance(20_000);
         savingsAccount.add(500);
         savingsAccount.pay(20501);
 
-        checkingAccount.setNameOwner("Julia");
+        checkingAccount.setNameOwner("James");
         checkingAccount.setBalance(300_000);
         checkingAccount.add(200_000);
         checkingAccount.pay(20_000);
 
-        creditAccount.setNameOwner("Lukas");
+        creditAccount.setNameOwner("James");
         creditAccount.setBalance(0);
         creditAccount.add(2_000);
         creditAccount.pay(20_000);
         creditAccount.add(2_000);
         creditAccount.add(17_999);
         creditAccount.add(19_000);
+
+        client.addNewAccount(checkingAccount);
+        client.addNewAccount(creditAccount);
+        client.addNewAccount(savingsAccount);
+        client.pay(480000);
+
     }
 }
