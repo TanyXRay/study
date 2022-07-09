82d0264d1f873d2afc26da82ce6e6bdfb3b277c1
User <tany.taray@gmail.com>
2022-07-09 23:37:52 +0400

    task with array

---
+++ src/study/javarush/practicum/arrays/Array15.java
@@ -0,0 +1,64 @@
+package study.javarush.practicum.arrays;
+
+import java.util.Scanner;
+
+/**
+ * Решение задачи №1 по теме "Одномерные массивы".
+ * Решение задачи №1 по теме "Исключения".
+ */
+
+public class Array15 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] products = {"Хлеб", "Молоко", "Яблоки"};
+        int[] prices = {35, 87, 109};
+        int[] productsCount = new int[3];
+
+        System.out.println("Список возможных товаров для покупки: ");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " руб/шт.");
+        }
+        while (true) {
+            System.out.println("Выберите товар и количество или введите \"end\" ");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("end")) {
+                break;
+            }
+            String[] parts = input.split(" ");
+            if (parts.length != 2) {
+                continue;
+            }
+            try {
+                int productNumber = Integer.parseInt(parts[0]) - 1; // выбор продукта
+                if (productNumber >= 3 || productNumber < 0) {
+                    System.out.println("Вы ввели некорректное число продукта. Попробуйте снова!");
+                    continue;
+                }
+                int productCount = Integer.parseInt(parts[1]); //  выбор количества продуктов
+                if (productCount > 100 || productCount <= 0) {
+                    System.out.println("Вы ввели некорректное кол-во продукта. Попробуйте снова!");
+                    continue;
+                }
+                productsCount[productNumber] += productCount;
+            } catch (NumberFormatException e) {
+                System.out.println("Вы ввели текст заместо числа. Попробуйте снова!");
+                continue;
+            }
+        }
+
+        System.out.println("Ваша корзина:");
+        int sum = 0;
+        for (int i = 0; i < productsCount.length; i++) {
+            int allCountProduct = productsCount[i];
+            int priceSumByProduct = prices[i] * allCountProduct;
+            if (allCountProduct > 0) {
+                System.out.println(products[i] + " " + allCountProduct + " шт. в сумме " + priceSumByProduct + " руб.");
+                sum += priceSumByProduct;
+            }
+        }
+        System.out.println("Итого: " + sum + " руб.");
+        scanner.close();
+    }
+}
