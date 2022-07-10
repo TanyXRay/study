4342f25b2fe93438a41771139e864eff26568e4e
User <tany.taray@gmail.com>
2022-07-10 18:26:23 +0400

    task with inner class

---
+++ src/study/javarush/practicum/innerclass/Car.java
@@ -0,0 +1,23 @@
+package study.javarush.practicum.innerclass;
+
+/**
+ * Работа с вложенными классами.
+ * Задача: создать вложенный класс Engine, а в нем уже прописать поле boolean isRunning
+ * и методы start(),stop().
+ */
+
+public class Car {
+    private Engine engine;
+
+    public static class Engine {
+        private boolean isRunning;
+
+        public void start() {
+            isRunning = true;
+        }
+
+        public void stop() {
+            isRunning = false;
+        }
+    }
+}
